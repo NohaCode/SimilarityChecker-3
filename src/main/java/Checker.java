c6c44e250ef3af6936d4a5c6027565fecb9d9bd5
jinha <gammazx@naver.com>
2024-04-29 14:22:09 +0900

    [feature:length] - fetaure2 : 2twice length

--- src/main/java/Checker.java
+++ src/main/java/Checker.java
@@ -4,6 +4,9 @@ public class Checker {
 
     public int lengthCheck(String str1, String str2) {
         if(str1.length() == str2.length()) return 60;
-        return 0;
+        else if(str1.length() >= 2*str2.length() || 2*str1.length() <= 2*str2.length()){
+            return 0;
+        }
+        return 30;
     }
 }
--- src/test/java/CheckerTest.java
+++ src/test/java/CheckerTest.java
@@ -20,4 +20,15 @@ class CheckerTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void LengthCheckTest2() {
+        Checker checker = new Checker();
+        int actual = checker.lengthCheck("A", "BB");
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+
 }
