b07d3dd18a1034adc8382d7f9782a67064925489
jinha <gammazx@naver.com>
2024-04-29 14:23:24 +0900

    [feature:length] - refactoring2 : 2twice length

--- src/main/java/Checker.java
+++ src/main/java/Checker.java
@@ -4,9 +4,13 @@ public class Checker {
 
     public int lengthCheck(String str1, String str2) {
         if(str1.length() == str2.length()) return 60;
-        else if(str1.length() >= 2*str2.length() || 2*str1.length() <= 2*str2.length()){
+        else if(isLengthTwice(str1, str2)){
             return 0;
         }
         return 30;
+    }
+
+    private static boolean isLengthTwice(String str1, String str2) {
+        return str1.length() >= 2 * str2.length() || 2 * str1.length() <= 2 * str2.length();
     }
 }
