70194b1dabdcb1f884ec1139fa3e9d6cadca8c5e
jinha <gammazx@naver.com>
2024-04-29 14:38:03 +0900

    [feature:length] - feature3 : plus some point

--- src/main/java/Checker.java
+++ src/main/java/Checker.java
@@ -2,12 +2,24 @@ public class Checker {
     public Checker() {
     }
 
-    public int lengthCheck(String str1, String str2) {
-        if(str1.length() == str2.length()) return 60;
-        else if(isLengthTwice(str1, str2)){
+    public double lengthCheck(String str1, String str2) {
+        if (str1.length() == str2.length()) return 60;
+        else if (isLengthTwice(str1, str2)) {
             return 0;
+        } else {
+            double A, B, gap = 0;
+            if (str1.length() > str2.length()) {
+                A = str1.length();
+                B = str2.length();
+                gap = A - B;
+            } else {
+                A = str2.length();
+                B = str1.length();
+                gap = B - A;
+            }
+            //System.out.println(Math.round(60 - 60 * gap / B));
+            return Math.round(60 - 60 * gap / B);
         }
-        return 30;
     }
 
     private static boolean isLengthTwice(String str1, String str2) {
--- src/test/java/CheckerTest.java
+++ src/test/java/CheckerTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -7,26 +8,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CheckerTest {
 
-    @Test
-    void CheckerTest() {
-        Checker checker = new Checker();
+    private Checker checker;
+
+    @BeforeEach
+    void setUp() {
+        checker = new Checker();
     }
 
     @Test
     void LengthCheckTest1() {
-        Checker checker = new Checker();
-        int actual = checker.lengthCheck("ASD", "DSA");
+        double actual = checker.lengthCheck("ASD", "DSA");
         int expected = 60;
-
         assertEquals(expected, actual);
     }
 
     @Test
     void LengthCheckTest2() {
-        Checker checker = new Checker();
-        int actual = checker.lengthCheck("A", "BB");
+        double actual = checker.lengthCheck("A", "BB");
         int expected = 0;
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    void LengthCheckTest3() {
+        double actual = checker.lengthCheck("AAABB", "BAA");
+        int expected = 20;
         assertEquals(expected, actual);
     }
 
