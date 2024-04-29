476cc28c20ef0c6f2596867933ceb835a761d02a
jinha <gammazx@naver.com>
2024-04-29 14:56:14 +0900

    [feature:length] - test all

--- src/test/java/CheckerTest.java
+++ src/test/java/CheckerTest.java
@@ -16,30 +16,60 @@ class CheckerTest {
     }
 
     @Test
-    void LengthCheckTest1() {
+    void LengthCheckTest_WhenSameLength1() {
         double actual = checker.lengthCheck("ASD", "DSA");
         int expected = 60;
         assertEquals(expected, actual);
     }
 
     @Test
-    void LengthCheckTest2() {
+    void LengthCheckTest_WhenSameLength2() {
+        double actual = checker.lengthCheck("DSA", "ASD");
+        int expected = 60;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void LengthCheckTest_WhenLengthTwice1() {
         double actual = checker.lengthCheck("A", "BB");
         int expected = 0;
         assertEquals(expected, actual);
     }
 
     @Test
-    void LengthCheckTest3() {
+    void LengthCheckTest_WhenLengthTwice2() {
+        double actual = checker.lengthCheck( "BB", "A");
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+
+
+    @Test
+    void LengthCheckTest_WhenSubPoint1() {
         double actual = checker.lengthCheck("AAABB", "BAA");
         int expected = 20;
         assertEquals(expected, actual);
     }
 
     @Test
-    void LengthCheckTest4() {
+    void LengthCheckTest_WhenSubPoint2() {
         double actual = checker.lengthCheck("BAA", "AAABB");
         int expected = 20;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void LengthCheckTest_WhenSubPoint3() {
+        double actual = checker.lengthCheck("AA", "AAE");
+        int expected = 30;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void LengthCheckTest_WhenSubPoint4() {
+        double actual = checker.lengthCheck("AAE", "AA");
+        int expected = 30;
         assertEquals(expected, actual);
     }
 
