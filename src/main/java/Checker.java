e7f7fea33ded874098589efb6f3a72ccf394c20e
jinha <gammazx@naver.com>
2024-04-29 14:17:20 +0900

    [feature:length] - feture1

--- src/main/java/Checker.java
+++ src/main/java/Checker.java
@@ -2,4 +2,7 @@ public class Checker {
     public Checker() {
     }
 
+    public int lengthCheck(String asd, String dsa) {
+        return 60;
+    }
 }
--- src/test/java/CheckerTest.java
+++ src/test/java/CheckerTest.java
@@ -1,5 +1,8 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CheckerTest {
@@ -7,5 +10,14 @@ class CheckerTest {
     @Test
     void CheckerTest() {
         Checker checker = new Checker();
+    }
+
+    @Test
+    void LengthCheckTest1() {
+        Checker checker = new Checker();
+        int expected = 60;
+        int actual = checker.lengthCheck("ASD", "DSA");
+
+        assertEquals(expected, actual);
     }
 }
