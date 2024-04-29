0b649493e7aeb687de6fc9ebcb0d179f7bcd7286
jinha <gammazx@naver.com>
2024-04-29 14:09:35 +0900

    initial Setting

--- src/main/java/Checker.java
+++ src/main/java/Checker.java
@@ -1,2 +1,5 @@
 public class Checker {
+    public Checker() {
+    }
+
 }
--- src/test/java/CheckerTest.java
+++ src/test/java/CheckerTest.java
@@ -1,5 +1,11 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CheckerTest {
 
+    @Test
+    void CheckerTest() {
+        Checker checker = new Checker();
+    }
 }
